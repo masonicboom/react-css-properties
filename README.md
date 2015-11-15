686e0ebc7aa0ea39a320e19a771e3dc737733ecd
masonicboom <masonicboom@users.noreply.github.com>
2015-11-14 21:14:22 -0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+reference.csv comes from http://docs.webplatform.org/w/index.php?title=Special:Ask&offset=0&limit=500&q=%5B%5BCategory%3ACSS+Properties%5D%5D&p=format%3Dtemplate%2Flink%3Dnone%2Fsearchlabel%3DSee-20more-20pages...%2Ftemplate%3DSummary_Table_Body%2Fintrotemplate%3DSummary_Table_Header%2Foutrotemplate%3DSummary_Table_Footer&po=%3FPage+Title%0A%3FSummary%0A#, and is used under license (http://docs.webplatform.org/wiki/Template:CC-by-3.0).
+
+To generate property-types.txt:
+
+		node process.js > property-types.txt
---
+++ process.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var camelize = require('camelize');
+
+// These are extracted from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/react/react.d.ts#L427.
+var existingProperties = {
+	boxFlex: true,
+	boxFlexGroup: true,
+	columnCount: true,
+	flex: true,
+	flexGrow: true,
+	flexShrink: true,
+	fontWeight: true,
+	lineClamp: true,
+	lineHeight: true,
+	opacity: true,
+	order: true,
+	orphans: true,
+	widows: true,
+	zIndex: true,
+	zoom: true,
+	fontSize: true,
+	fillOpacity: true,
+	strokeOpacity: true,
+	strokeWidth: true
+}
+
+var d = fs.readFileSync('reference.csv');
+parse(d, function(err, data) {
+	var seenHeader = false;
+	for (var row of data) {
+		if (seenHeader) {
+			var propertyName = row[0];
+			var p = camelize(propertyName);
+			
+			if (existingProperties[p]) {
+				continue;
+			}
+			
+			console.log(`${p}?: any;`);
+		}
+		seenHeader = true;
+	}
+});
